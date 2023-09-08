4775774345a040e70600a79531dee2e0bc88794f
TheCrixS <csotov@ucentral.edu.co>
2023-09-07 20:27:11 -0500

    Cambio de programa, adicion de metodos de conversion

--- src/operaciones/Conversion.java
+++ src/operaciones/Conversion.java
@@ -7,4 +7,16 @@ public class Conversion {
     public int binariosDecimales(int num1, int num2){
         return 0;
     }
+    public int decimalOctal (int num1, int num2){
+        return 0;
+    }
+    public int octalDecimal(int num1, int num2){
+        return 0;
+    }
+    public String decimalHexadecimal (int num1, int num2){
+        return "";
+    }
+    public String hexadecimalDecimal(int num1, int num2){
+        return "";
+    }
 }
