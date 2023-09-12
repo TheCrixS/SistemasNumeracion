76df577002d0eaf94b09081bff72663e8ec6dbe3
TheCrixS <csotov@ucentral.edu.co>
2023-09-11 20:26:09 -0500

    Metodo decimalBinarios

--- src/operaciones/Conversion.java
+++ src/operaciones/Conversion.java
@@ -1,22 +1,34 @@
 package operaciones;
 
 public class Conversion {
-    public int decimalesBinarios(int num1, int num2){
-        return 0;
-    }
-    public int binariosDecimales(int num1, int num2){
-        return 0;
-    }
-    public int decimalOctal (int num1, int num2){
-        return 0;
-    }
-    public int octalDecimal(int num1, int num2){
-        return 0;
-    }
-    public String decimalHexadecimal (int num1, int num2){
+    public String decimalesBinarios(int decimal){
+        if (decimal == 0){
+            return "0";
+        }else {
+            StringBuilder binario = new StringBuilder();
+            int cont = decimal;
+            while (cont > 0){
+                int residuo = cont % 2;
+                binario.insert(0,residuo);
+                cont /= 2;
+                System.out.println(decimal+" / 2 = "+cont+", Residuo = "+residuo);
+            }
+        }
         return "";
     }
-    public String hexadecimalDecimal(int num1, int num2){
+    public String binariosDecimales(int binario){
+        return "";
+    }
+    public String decimalOctal (int decimal){
+        return "";
+    }
+    public String octalDecimal(int octal){
+        return "";
+    }
+    public String decimalHexadecimal (int decimal){
+        return "";
+    }
+    public String hexadecimalDecimal(int hdecimal){
         return "";
     }
 }
